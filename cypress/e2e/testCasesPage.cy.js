f5c28f04c32536e5e4850c77da9ee166f8e456f8
meliscula <carloray30@gmail.com>
2025-06-13 15:49:17 +0800

    Added Test Cases Page Verification

---
+++ cypress/e2e/testCasesPage.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+import { loginSetup} from "../support/utilities/hooks";
+
+describe('Test Cases Page', () => {
+    beforeEach(function() {
+        loginSetup();
+    });
+
+    it('should display the test cases page', function() {
+        cy.fixture('loginData').then((userData) => {
+
+            // Navigate to Test Cases page
+            cy.get('a[href="/test_cases"]').contains('Test Cases').click();
+            cy.getByText('Test Cases').should('be.visible');
+
+            // Verify the presence of test case elements
+            cy.getByText('Test Case 1').should('be.visible');
+            cy.getByText('Test Case 2').should('be.visible');
+            cy.getByText('Test Case 3').should('be.visible');
+
+            // Verify that the test cases are listed correctly
+            cy.get('#form > .container').children('.panel-group').should('exist');
+        });
+    });
+
+
+});
